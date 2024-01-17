97e530331f3ce6a6880ea9d711e1e32c909af899
1stby <love970120@gmail.com>
2024-01-17 23:41:12 +0800

    add 表單驗證 修改錯誤

--- form_validator/script.js
+++ form_validator/script.js
@@ -1,45 +1,45 @@
 const form = document.getElementById("form");
-const Username = document.getElementById("Username");
+const username = document.getElementById("username");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 const email = document.getElementById("email");
 
 // input 錯誤的訊息
-function showError(input,message){
+function showError(input, message) {
     const formControl = input.parentElement;//找input的父元素
     formControl.className = "form-control error";
-    const small =formControl.querySelector("small");
+    const small = formControl.querySelector("small");
     small.innerText = message;
 }
 //Show success outline 正確
-function showSuccess (input){
+function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = "form-control success"
 }
 
 // 檢查email 是否正確
-function checkEmail(input){
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value)){
+    if (re.test(input.value)) {
         showSuccess(input);
-    }else{
-        showError(input,"Email is not valid")
+    } else {
+        showError(input, "Email is not valid")
     }
 }
 // 檢查 是否為必填
-function checkRequired(inputArr){
-    inputArr.forEach(function (input){
-         //trim 用於去除前後空格
-        if (input.value.trim() ===""){
-            showError(input,`${getFieldName(input)} is required`);
-        }else{
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        //trim 用於去除前後空格
+        if (input.value.trim() === "") {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
             showSuccess(input);
         }
     });
 }
 // check input length
-function checkLength(input, min, max){
-    if (input.value.length< min){
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
         showError(
             input,
             `${getFieldName(input)} must be at least ${min} characters`
@@ -49,20 +49,20 @@ function checkLength(input, min, max){
             input,
             `${getFieldName(input)} must be less than ${max} characters`
         );
-    } else{
+    } else {
         showSuccess(input);
     }
 }
 // check password 是否一樣
-function checkPassword(input1,input2){
-    if(input1.value !== input2.value){
-        showError(input2,"Passwords do not match")
+function checkPassword(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Passwords do not match")
     }
 }
 // Get fieldname
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-  }
+}
 //Event Listeners
 form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -71,4 +71,4 @@ form.addEventListener("submit", function (e) {
     checkLength(password, 6, 25);
     checkEmail(email);
     checkPassword(password, password2);
-  });
+});
